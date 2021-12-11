a4e7b8fdc794cf9d436851d7ea6f0ff7d187df74
chamrongthordev <chamrongthor.dev@gmail.com>
2021-12-11 20:25:52 +0700

    Meteor vue one binding V-bind

---
+++ 5-meteor-vue-v-binding/imports/ui/App.vue
@@ -0,0 +1,32 @@
+<!--View-->
+<template>
+  <!--ROOT element-->
+  <div>
+    <h1>Hello</h1>
+    <!--One-binding: from view to controller-->
+    <input type="text" v-bind="name">
+    <p>{{message}} {{name}}</p>
+  </div>
+</template>
+<!--Controller-->
+<script>
+export default {
+  /**
+   * @data method used for returning data.
+   * @returns {{name: string, message: string}}
+   */
+  data() {
+    return {
+      message: "Hello, ",
+      name: "Unknown Name"
+    }
+  }
+}
+</script>
+
+<!--Style-->
+<style>
+  body{
+    background: green;
+  }
+</style>
