d65ab889886d3a76afd5025c8c7444fbdcfac34b
chamrongthordev <chamrongthor.dev@gmail.com>
2021-12-11 20:32:05 +0700

    meteor-vue-visible-element-v-show

---
+++ 5-meteor-vue-visible-element-v-show/README.md
@@ -0,0 +1,7 @@
+# Introduction to Vue.js
+- Declaring variable and output
+- Conditionals and loops
+- Handling user input
+- Computed properties and watches
+- Class and style bindings
+- Components
---
+++ 5-meteor-vue-visible-element-v-show/imports/ui/App.vue
@@ -0,0 +1,13 @@
+<!--View-->
+<template>
+  <!--ROOT element-->
+  <div>
+    <h1 v-show="false">Hello</h1>
+    <h1 v-show="true">World!</h1>
+  </div>
+</template>
+<!--Controller-->
+<script>
+export default {
+}
+</script>
