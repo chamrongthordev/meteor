ca07f15f96dc21a0b89b90bc2ba4c9e71f66af24
chamrongthordev <chamrongthor.dev@gmail.com>
2021-12-11 20:20:55 +0700

    Meteor vue two binding v-model

---
+++ 4-meteor-vue-two-binding-v-model/imports/ui/App.vue
@@ -0,0 +1,32 @@
+<!--View-->
+<template>
+  <!--ROOT element-->
+  <div>
+    <h1>Hello</h1>
+    <!--Two-binding: from controller to view and voice-versa-->
+    <input type="text" v-model="name">
+    <p>{{message}} {{name}}</p>
+  </div>
+</template>
+<!--Controller-->
+<script>
+export default {
+  /**
+   * @data method used for returning data.
+   * @returns {{name: string, message: string}}
+   */
+  data() {
+    return {
+      message: "Hello, ",
+      name: "Unknown Name"
+    }
+  }
+}
+</script>
+
+<!--Style-->
+<style>
+  body{
+    background: green;
+  }
+</style>
