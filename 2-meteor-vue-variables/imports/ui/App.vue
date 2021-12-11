2f21e92789d9c9b341fea55aae427f384f4209c3
chamrongthordev <chamrongthor.dev@gmail.com>
2021-12-11 19:48:52 +0700

    Declear variable in Vue and data() usage

---
+++ 2-meteor-vue-variables/imports/ui/App.vue
@@ -0,0 +1,16 @@
+
+<!--LOGIC-->
+<script>
+export default {
+  /**
+   * @data method used for returning data.
+   * @returns {{name: string, message: string}}
+   */
+  data() {
+    return {
+      message: "Hello, Vue!",
+      name: "Chamrong"
+    }
+  }
+}
+</script>
