b6efe1df1cfbd24823ffb46dac39e1c6c800f9a7
chamrongthordev <chamrongthor.dev@gmail.com>
2021-12-11 20:41:53 +0700

    meteor-vue-design-input-form

---
+++ 6-meteor-vue-design-input-form/imports/ui/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <form>
+      <label for="firstName">First Name:</label>
+      <input type="text" id="firstName" v-bind="firstName">
+      <label for="lastName">Last Name:</label>
+      <input type="text" id="lastName" v-bind="lastName">
+      <button type="submit">Submit</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return{
+      firstName: 'Chamrong',
+      lastName: 'Thor'
+    }
+  }
+}
+</script>
